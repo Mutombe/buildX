82967fdbaf46143d18f30a01d3769ec6206945bc
Mutombe <simbarashemutombe1@gmail.com>
2024-09-07 13:36:15 +0200

    refactoring booking

--- bookings/models.py
+++ bookings/models.py
@@ -9,7 +9,7 @@ class Booking(models.Model):
     """
     Booking model
     """
-
+    booking_type = models.CharField(max_length=50)
     unit = models.ForeignKey(Unit, null=True, blank=True, on_delete=models.CASCADE)
     property = models.ForeignKey(Property, null=True, blank=True, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
@@ -17,36 +17,13 @@ class Booking(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     start_date = models.DateField(default=date.today)
     end_date = models.DateField(null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         if self.unit:
             return f"Booking by {self.customer.username} for {self.unit.name}"
         else:
             return f"Booking by {self.customer.username} for {self.property.name}"
-
-    def approve(self):
-        self.status = 'approved'
-        self.unit.occupied = True
-        self.unit.save()
-        self.save()
-        send_mail(
-            'Booking Approved',
-            f'Your booking for {self.unit.name} at {self.unit.location} has been approved. You can procede to make your initial payment',
-            'simbarashemutombe1@gmail.com',
-            [self.customer.email],
-            fail_silently=False,
-        )
-
-    def deny(self):
-        self.status = 'denied'
-        self.save()
-        send_mail(
-            'Booking Denied',
-            f'Your booking for {self.unit.name} has been denied. You can open the property owner messaging box for more info',
-            'simbarashemutombe1@gmail.com',
-            [self.customer.email],
-            fail_silently=False,
-        )
 
 
 class Notification(models.Model):
